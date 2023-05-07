3c59955141e0610497111e37f2273c41ceee743d
xcode73 <xcode.uln@gmail.com>
2023-05-07 16:27:22 +0400

    feat(LocationDetailViewController): add pageControl
    This adds pageControl component.

--- WeatherMS/LocationDetailViewController.swift
+++ WeatherMS/LocationDetailViewController.swift
@@ -15,6 +15,7 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     var weatherLocation: WeatherLocation!
     var locationIndex = 0
@@ -49,6 +50,9 @@ class LocationDetailViewController: UIViewController {
         placeLabel.text = weatherLocation.name
         temperatureLabel.text = "--°"
         summaryLabel.text = ""
+        
+        pageControl.numberOfPages = pageViewController.weatherLocations.count
+        pageControl.currentPage = locationIndex
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -74,6 +78,21 @@ class LocationDetailViewController: UIViewController {
         
         pageViewController.weatherLocations = source.weatherLocations
         pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: locationIndex)], direction: .forward, animated: false)
+    }
+    
+    @IBAction func pageControlTapped(_ sender: UIPageControl) {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+                      
+        let pageViewController = window?.rootViewController as! PageViewController
+        
+        var direction: UIPageViewController.NavigationDirection = .forward
+        
+        if sender.currentPage < locationIndex {
+            direction = .reverse
+        }
+        pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: sender.currentPage)], direction: direction, animated: true)
     }
 }
 
